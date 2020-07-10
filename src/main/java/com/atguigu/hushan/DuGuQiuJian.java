99ebacd46fc40031b62916ae5ecf2af6902ce1f4
wangzijian <wangzijian0518@outlook.com>
2020-07-10 23:05:21 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/hushan/DuGuQiuJian.java
@@ -0,0 +1,9 @@
+package com.atguigu.hushan;
+
+public class DuGuQiuJian {
+    public static void main(String[] args) {
+        System.out.println("dugu1");
+        System.out.println("dugu2");
+        System.out.println("dugu3");
+    }
+}
