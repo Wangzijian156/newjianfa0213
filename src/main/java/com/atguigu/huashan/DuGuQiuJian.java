8c47e17ad94dc9cb6c5c1a06c78a82fc5adaa026
wangzijian159 <2466230236@qq.com>
2020-07-12 09:43:42 +0800

    xixingdafa

--- src/main/java/com/atguigu/huashan/DuGuQiuJian.java
+++ src/main/java/com/atguigu/huashan/DuGuQiuJian.java
@@ -1,9 +1,11 @@
-package com.atguigu.hushan;
+package com.atguigu.huashan;
 
 public class DuGuQiuJian {
     public static void main(String[] args) {
         System.out.println("dugu1");
         System.out.println("dugu2");
         System.out.println("dugu3");
+        System.out.println("dugu4");
+        System.out.println("dugu5");
     }
 }
---
+++ src/main/java/com/atguigu/riyue/Xixingdafa.java
@@ -0,0 +1,4 @@
+package com.atguigu.riyue;
+
+public class Xixingdafa {
+}
